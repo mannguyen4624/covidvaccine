1adb787630127e1408120680a27666f9bcfa24a8
Man Nguyen <nguyen.man@northeastern.edu>
2021-05-31 21:18:55 -0400

    cookie

--- functions.js
+++ functions.js
@@ -1,3 +1,10 @@
+function readCookie() {
+    if (document.cookie.length > 0) {
+        let response = document.cookie.split("=")[1];
+        window.location.href = `${response}.html`;
+    }
+}
+
 let url = 'https://desolate-wave-83957.herokuapp.com/';
 
 document.onclick = function (event) {
@@ -5,22 +12,26 @@ document.onclick = function (event) {
     const target = 'target' in event ? event.target : event.srcElement;
 
     if (target.classList.contains("yes")) {
+        document.cookie = "response=yes";
         let updateUrl = url.concat("yes");
         fetch(updateUrl);
         window.location.href = "yes.html";
     } else if (target.classList.contains("no")) {
+        document.cookie = "response=no";
         let updateUrl = url.concat("no");
         fetch(updateUrl);
         window.location.href = "no.html";
     }
 }
 
+
+
 function getData(fromYes) {
     fetch(url)
-        .then(function(response) {
+        .then(function (response) {
             return response.json();
         })
-        .then(function(jsonResponse) {
+        .then(function (jsonResponse) {
             if (fromYes) {
                 document.getElementById("yes").innerText = `You are now 1 out of ${jsonResponse.yes} people who have gotten the Covid vaccine. :)`;
                 document.getElementById("no").innerText = `On the other hand, ${jsonResponse.no} people have not.`;
